feffdf9badf69450de5c4ca1f34b8678c9e9d01b
20161104595 <a20161104595@mac26.local>
2017-06-16 16:27:17 +0800

    Initial Commit

---
+++ SORT/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SORT
+//
+//  Created by 20161104595 on 17/6/16.
+//  Copyright © 2017年 20161104595. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
