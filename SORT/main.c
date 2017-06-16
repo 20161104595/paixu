8fd222862b117561715fc770aa237473c114e0e4
20161104595 <a20161104595@mac26.local>
2017-06-16 16:41:28 +0800

    sort
    文件排序

--- SORT/main.c
+++ SORT/main.c
@@ -6,10 +6,46 @@
 //  Copyright © 2017年 20161104595. All rights reserved.
 //
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main(int argc, const char * argv[])
+{
+    int i,a[10],j,t;
+    FILE *fp1;
+    FILE *fp2;
+    fp1=fopen("//Users//a20161104595//Desktop//SORT//SORT//input.txt", "r+");
+    fp2=fopen("//Users//a20161104595//Desktop//SORT//SORT//output.txt","w+");
+    if(fp2==NULL)
+    {
+        printf("打开文件错误，要打开的文件不存在");
+    }
+    else
+    {
+        for(i=0;i<10;i++)
+        {
+            fscanf(fp1,"%d",&a[i]);
+        }
+        for(j=0;j<9;j++)
+        {
+            for(i=0;i<=8-j;i++)
+            {
+                if(a[i]>a[i+1])
+                {
+                    t=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=t;
+                }
+            }
+        }
+        printf("排列后顺序为：");
+        fprintf(fp2,"排列后顺序为：");
+        for(i=0;i<10;i++)
+        {
+            printf("%d ",a[i]);
+        }
+        fprintf(fp2,"\n");
+        printf("\n");
+        fclose(fp1);
+        fclose(fp2);
+    }
     return 0;
 }
